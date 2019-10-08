d4df737cbc59f7ecdb8a51f4df99b745644db7fb
the-it-weirdo <ddsmegh4@gmail.com>
2019-10-08 22:10:46 +0530

    implements rotation on user touch.

--- Assets/Rotate.cs
+++ Assets/Rotate.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour {
 
-	public int roatateSpeed;
+	public int roatateSpeed = 50;
+    public float touchRotateSpeed;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +14,19 @@ public class Rotate : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (new Vector3 (0, Time.deltaTime * roatateSpeed, 0));
+
+        if (Input.touchCount == 0)
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * roatateSpeed, 0));
+        }
 	}
+
+    void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * touchRotateSpeed * Mathf.Deg2Rad;
+        float rotY = Input.GetAxis("Mouse Y") * touchRotateSpeed * Mathf.Deg2Rad;
+
+        transform.Rotate(Vector3.up, -rotX);
+        transform.Rotate(Vector3.right, rotY);
+    }
 }
